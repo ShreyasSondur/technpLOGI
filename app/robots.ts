75ba9ac0bfa20c51af287b558af166540b1a77bd
Mohammed Shimaz <mohammedshimaz87@gmail.com>
2025-12-14 22:40:30 +0530

    SEO

---
+++ app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://technologi.in/sitemap.xml",
+  };
+}
